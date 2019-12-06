e4275a8e07b565630aefba62d71fca28d8304060
Sergey <snugryumov@gmail.com>
2019-12-06 20:47:30 +0300

    project structure

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home");
+const postersRoutes = require("./routes/posters");
+const addRoutes = require("./routes/add");
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs"
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+app.use("/", homeRoutes);
+app.use("/posters", postersRoutes);
+app.use("/add", addRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
