0a06c84b44547e35c18ebc9317fec43471ba13d2
Sergey <snugryumov@gmail.com>
2019-12-07 12:55:31 +0300

    features: output posters on page

--- routes/posters.js
+++ routes/posters.js
@@ -1,12 +1,22 @@
 const { Router } = require("express");
+const Posters = require("../models/poster");
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("posters", {
-    title: "Posters",
-    isPosters: true
-  });
+  Posters.getAll()
+    .then(response => {
+      const posters = response;
+
+      return posters;
+    })
+    .then(posters => {
+      res.render("posters", {
+        title: "Posters",
+        isPosters: true,
+        posters
+      });
+    });
 });
 
 module.exports = router;
